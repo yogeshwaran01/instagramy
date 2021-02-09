895e6d645cc0cddf22a72fe50bb4b46ee69db2ca
YOGESHWARAN R <yogeshin247@gmail.com>
2021-02-09 22:06:11 +0530

    Added Some Instagram Analysing Functions

--- instagramy/plugins/analysis.py
+++ instagramy/plugins/analysis.py
@@ -1,27 +1,30 @@
 from instagramy.InstagramUser import InstagramUser
+from instagramy.InstagramHashTag import InstagramHashTag
 
 
-def analyze_users(usernames: list) -> dict:
+def analyze_users_popularity(usernames: list, sessionid: str) -> dict:
+    """ Functions return the required data to Analze Instagram users """
     followers = []
     following = []
     posts = []
     for username in usernames:
-        user = InstagramUser(username, from_cache=True)
+        user = InstagramUser(username, sessionid)
         followers.append(user.number_of_followers)
         following.append(user.number_of_followings)
         posts.append(user.number_of_posts)
     data = {
         "Usernames": usernames,
         "Followers": followers,
-        "Followings": following,
+        "Following": following,
         "Posts": posts,
     }
     return data
 
 
-def analyze_user_recent_posts(username: str) -> dict:
+def analyze_user_recent_posts(username: str, sessionid: str) -> dict:
+    """ Functions return the required data to Analze Instagram user recent post """
 
-    user = InstagramUser(username)
+    user = InstagramUser(username, sessionid)
     posts = user.posts
     urls = []
     likes = []
@@ -32,3 +35,14 @@ def analyze_user_recent_posts(username: str) -> dict:
         comments.append(post["comments"])
 
     return {"Posts": urls, "Likes": likes, "Comments": comments}
+
+
+def analyze_hashtags(hashtags: list, sessionid: str) -> dict:
+    """ Functions return the required data to Analze Instagram Hashtags """
+
+    posts = []
+    for hashtag in hashtags:
+        tag = InstagramHashTag(hashtag, sessionid)
+        posts.append(tag.number_of_posts)
+
+    return {"Hashtag": hashtags, "Posts": posts}
