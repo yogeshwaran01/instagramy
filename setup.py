e2a61b2b714c0e8a00549975c9ca5c13868edff1
YOGESHWARAN R <yogeshin247@gmail.com>
2021-02-11 10:24:53 +0530

    Updated the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="instagramy",
-    version="4.1",
+    version="4.2",
     license='MIT',
     author="Yogeshwaran R",
     author_email="yogeshin247@gmail.com",
