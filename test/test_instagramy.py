db26ab3b38342ec2d9fa0dafdf818268c01072dc
YOGESHWARAN R <yogeshin247@gmail.com>
2021-04-06 12:33:57 +0530

    Test case get updated

--- test/test_instagramy.py
+++ test/test_instagramy.py
@@ -1,16 +1,51 @@
-from instagramy import InstagramUser
-from instagramy import InstagramPost
-from instagramy import InstagramHashTag
+# Unable to fetch data from Instagram effectively
+# So, Some data already store in other website to test the parsers
+import unittest
+import json
+
+from instagramy.core.requests import get
+from instagramy.plugins.manual_loading import *
 
 
-def test_user():
-    user = InstagramUser('github')
-    assert user.username == "github"
+# loading sample data for test from other website
 
-def test_tag():
-    tag = InstagramHashTag('github')
-    assert tag.tagname == 'github'
+user_data = json.loads(get("https://yogeshwaran01.herokuapp.com/user_data"))
+post_data = json.loads(get("https://yogeshwaran01.herokuapp.com/post_data"))
+tag_data = json.loads(get("https://yogeshwaran01.herokuapp.com/tag_data"))
 
-def test_post():
-    post = InstagramPost('CGeYX2OA61s')
-    assert post.author == '@virat.kohli'
+
+class TestParsers(unittest.TestCase):
+    def test_InstagramUser(self):
+        user = InstagramUser(user_data)
+        self.assertEqual(user.biography, "Built for developers.")
+        self.assertIsNone(user.connected_fb_page)
+        self.assertFalse(user.followed_by_viewer)
+        self.assertFalse(user.follows_viewer)
+        self.assertEqual(user.fullname, "GitHub")
+        self.assertFalse(user.has_blocked_viewer)
+        self.assertFalse(user.has_country_block)
+        self.assertFalse(user.has_blocked_viewer)
+        self.assertFalse(user.is_blocked_by_viewer)
+        self.assertFalse(user.is_joined_recently)
+        self.assertFalse(user.is_private)
+        self.assertTrue(user.is_verified)
+        self.assertAlmostEqual(user.no_of_mutual_follower, 0)
+        self.assertAlmostEqual(user.number_of_followers, 139340)
+        self.assertAlmostEqual(user.number_of_followings, 20)
+        self.assertAlmostEqual(user.number_of_posts, 182)
+        self.assertEqual(user.username, "github")
+
+    def test_InstagramPost(self):
+        post = InstagramPost(post_data)
+        self.assertEqual(post.author, "chilll_memes")
+        self.assertEqual(
+            post.caption,
+            "Photo by CHILL MEMES in MRC Nagar with @memepattarai2.0. May be a meme of 3 people and text that says 'private Hospital nurse Chilll_ memes Govt Hospital nurse'.",
+        )
+        self.assertEqual(post.number_of_comments, 0)
+        self.assertEqual(post.number_of_likes, 21)
+
+    def test_InstagramHashtag(self):
+        tag = InstagramHashTag(tag_data)
+        self.assertEqual(tag.number_of_posts, 3600401)
+        self.assertEqual(tag.tagname, "python")
